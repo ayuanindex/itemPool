b8ca05b1842868b29e873decfe6ee2bee5bf781c
AYuan <lzy2118053626@hotmaill.com>
2020-03-13 09:35:19 +0800

    添加了btopic1

---
+++ btopic1/src/main/java/com/lenovo/btopic1/ApiService.java
@@ -0,0 +1,11 @@
+package com.lenovo.btopic1;
+
+/**
+ * @ProjectName: itemPool
+ * @Package: com.lenovo.btopic1
+ * @ClassName: ApiService
+ * @CreateDate: 2020/3/13 09:27
+ */
+public interface ApiService {
+
+}
