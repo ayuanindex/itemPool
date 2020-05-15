5261fc8136de46b504150262145ae5f8200fb182
AYuan <lzy2118053626@hotmaill.com>
2020-05-15 22:07:29 +0800

    添加了bTopic12

---
+++ btopic12/src/main/java/com/lenovo/btopic12/MainActivity.java
@@ -0,0 +1,33 @@
+package com.lenovo.btopic12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lenovo.basic.base.act.BaseActivity;
+
+/**
+ * @author ayuan
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutIdRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
